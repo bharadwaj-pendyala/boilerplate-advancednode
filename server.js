a59fbeda3cf710187d5002e14cdb941f18e9d7b3
bharadwajp <bharadwajpendyala@gmail.com>
2018-11-08 21:58:14 +0530

    Added solution for challenges

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-
-const app = express();
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const fccTesting    = require('./freeCodeCamp/fcctesting.js');
+const session       = require('express-session');
+const passport      = require('passport');
+const db            = require('mongodb');
+const mongo         = require('mongodb').MongoClient;
+const ObjectID      = require('mongodb').ObjectID;
+const app           = express();
+const LocalStrategy = require('passport-local');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -14,8 +19,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.route('/')
   .get((req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug', {title: 'Hello', message: 'Please login'});
   });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
 });
+
+app.set('view engine', 'pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+   done(null, user._id);
+ });
+                       
+passport.deserializeUser((id, done) => {
+        db.collection('users').findOne(
+            {_id: new ObjectID(id)},
+            (err, doc) => {
+              
+              //Maitaining a persistent connection with DB instead of done(null, null)
+              mongo.connect(process.env.DATABASE, (err,db) => {
+                  if(err) {
+                        console.log('Database error: ' + err);
+                    } else {
+                        app.listen();
+                        console.log('Successful database connection');
+                    }
+              });          
+           }
+      );       
+});                       
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    db.collection('users').findOne({ username: username }, function (err, user) {
+      console.log('User '+ username +' attempted to log in.');
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
